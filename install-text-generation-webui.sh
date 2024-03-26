2996c51fa95db5e54ff8206fa32750f4c8761f69
dceoy <dnarsil+github@gmail.com>
2024-03-27 00:00:00 +0900

    Fix bugs

--- install-text-generation-webui.sh
+++ install-text-generation-webui.sh
@@ -2,6 +2,11 @@
 
 set -euox pipefail
 
+head -20 /etc/*-release
+df -Th
+free -g
+nvidia-smi || :
+
 case "${OSTYPE}" in
   darwin* )
     brew update -q
@@ -19,27 +24,32 @@ case "${OSTYPE}" in
     ;;
 esac
 
-if [[ ! -d text-generation-webui ]]; then
-  aria2c \
-    https://raw.githubusercontent.com/dceoy/print-github-tags/master/print-github-tags
-  chmod +x print-github-tags
-  ./print-github-tags \
-    --release --latest --tar oobabooga/text-generation-webui \
-    | xargs -t aria2c -o text-generation-webui.tar.gz
-  tar xvf text-generation-webui.tar.gz
-  rm -f text-generation-webui.tar.gz
-  mv text-generation-webui-* text-generation-webui
-fi
+[[ ! -d text-generation-webui ]] || rm -rf text-generation-webui
+curl -sSLO https://raw.githubusercontent.com/dceoy/print-github-tags/master/print-github-tags
+chmod +x print-github-tags
+./print-github-tags --release --latest --tar oobabooga/text-generation-webui \
+  | xargs -t curl -sSL -o text-generation-webui.tar.gz
+tar xf text-generation-webui.tar.gz && rm -f text-generation-webui.tar.gz
+mv text-generation-webui-* text-generation-webui
 
 pip install -q --no-cache-dir -r text-generation-webui/requirements.txt
 
 LLM_URLS=( \
-  'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/resolve/main/llama-2-70b-chat.Q5_K_M.gguf' \   # 48.8 GB
-  'https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q5_K_M.gguf' \  # 32.2 GB
-  'https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-13b-fast-instruct-gguf/blob/main/ELYZA-japanese-Llama-2-13b-fast-instruct-q8_0.gguf' \  # 14 GB
-  'https://huggingface.co/mmnga/RakutenAI-7B-chat-gguf/blob/main/RakutenAI-7B-chat-q8_0.gguf' \  # 7.84 GB
+  # 30.6 GB
+  'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/resolve/main/llama-2-70b-chat.Q5_K_S.gguf' \
+  # 48.8 GB
+ 'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/resolve/main/llama-2-70b-chat.Q5_K_M.gguf' \
+  # 36.7 GB, 36.6 GB
+  # 'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/resolve/main/llama-2-70b-chat.Q8_0.gguf-split-a' \
+  # 'https://huggingface.co/TheBloke/Llama-2-70B-Chat-GGUF/resolve/main/llama-2-70b-chat.Q8_0.gguf-split-b' \
+  # 32.2 GB
+  'https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main/mixtral-8x7b-instruct-v0.1.Q5_K_M.gguf' \
+  # 14 GB
+  'https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-13b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-13b-fast-instruct-q8_0.gguf' \
+  # 7.84 GB
+  'https://huggingface.co/mmnga/RakutenAI-7B-chat-gguf/resolve/main/RakutenAI-7B-chat-q8_0.gguf' \
 )
 
 for u in "${LLM_URLS[@]}"; do
-  aria2c --dir text-generation-webui/models --console-log-level=warn -x 16 -s 16 -k 1M "${u}"
+  aria2c --dir text-generation-webui/models --console-log-level=warn -x 16 -s 16 -k 1M "${u}" -o "$(basename "${u}")"
 done
